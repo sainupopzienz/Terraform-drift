a234cf0f330f8409b81436764d3a47927ab3c485
sainupopzienz <sainusainu514@gmail.com>
2025-11-09 20:06:16 +0530

    Add key pair resource to Terraform

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -8,6 +8,11 @@ data "aws_ami" "amazon_linux" {
   }
 }
 
+resource "aws_key_pair" "main" {
+  key_name   = var.key_name
+  public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDRYhukv1rtE9W9epFiX5QTRxxx8XtviMi/X9NcfAwj5BhUY4bDJOvmJeN8SS16acMXXjwSBowv7Eu40vmAcApImZ25l4vtSaXAIltomKovA02Ca97Z35y7rOm29oa5brR4sblcVxp2AUR3vz6vI3ZQpUhCYjxdlt/eCor53uxG2xhM0RNyOt+i59AUXQDe/SDKLdJEKvtysrok4yVbPRPIgBWp8tKUKbGQpagAwoe57XrJqsB+h7cecdoL2KyX6EmvcSv6q6SeBClgVpHgffgk/MJXFp/VFCoux4K8t8AE+rwGUy7sm3GQZ2EhA5CIqhTDNX/wbUfn3m2qTIeinT4r"
+}
+
 resource "aws_security_group" "ec2" {
   name_prefix = "terraform-drift-ec2-"
   vpc_id      = var.vpc_id
@@ -43,7 +48,7 @@ resource "aws_security_group" "ec2" {
 resource "aws_instance" "main" {
   ami                    = data.aws_ami.amazon_linux.id
   instance_type          = var.instance_type
-  key_name              = var.key_name
+  key_name              = aws_key_pair.main.key_name
   vpc_security_group_ids = [aws_security_group.ec2.id]
   subnet_id             = var.public_subnet_ids[0]
 
