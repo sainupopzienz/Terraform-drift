83d0da5cd6e0576104827824e95c72432b3d3b5b
sainupopzienz <sainusainu514@gmail.com>
2025-11-09 21:13:47 +0530

    Change S3 backend bucket name for pipeline

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "terraform-state-196024211776"
+    bucket = "terraform-state-drift-pipeline-196024211776"
     key    = "terraform-drift-test/terraform.tfstate"
     region = "us-east-1"
   }
